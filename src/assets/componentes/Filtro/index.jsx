39a1cfbc5102a439f0d160859a5c33588e9a8134
Brenno Sousa Peres <bsousaperes.com>
2025-03-24 10:42:01 -0300

    fist commit

---
+++ src/assets/componentes/Filtro/index.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+export default function Filtro() {
+    return (
+        <section className="container-filtro">
+            <ul>
+                <li>Programação</li>
+            </ul>
+            <ul>
+                <li>React</li>
+            </ul>
+            <ul>
+                <li>Programação</li>
+            </ul>
+
+            <button>Acessibilidade</button>
+        </section>
+    )
+}
